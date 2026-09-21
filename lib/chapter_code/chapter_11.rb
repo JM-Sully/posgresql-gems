f3f416eedd008ff530eeae4137ede9e7b81d61f7
Jess Sully <45972923+JM-Sully@users.noreply.github.com>
2026-09-21 07:41:34 +0100

    Copy the migration files from the gem source to the application source

--- lib/chapter_code/chapter_11.rb
+++ lib/chapter_code/chapter_11.rb
@@ -18,3 +18,13 @@ Created database 'posgresql_gems_test'
 
 # create a new repository on github
 gh repo create posgresql-gems --public --source=. --remote=origin --push
+
+# copy the migration files from the gem source to the application source
+bin/rails acts_as_taggable_on_engine:install:migrations
+Copied migration 20260921063501_acts_as_taggable_on_migration.acts_as_taggable_on_engine.rb from acts_as_taggable_on_engine
+Copied migration 20260921063502_add_missing_unique_indices.acts_as_taggable_on_engine.rb from acts_as_taggable_on_engine
+Copied migration 20260921063503_add_taggings_counter_cache_to_tags.acts_as_taggable_on_engine.rb from acts_as_taggable_on_engine
+Copied migration 20260921063504_add_missing_taggable_index.acts_as_taggable_on_engine.rb from acts_as_taggable_on_engine
+Copied migration 20260921063505_change_collation_for_tag_names.acts_as_taggable_on_engine.rb from acts_as_taggable_on_engine
+Copied migration 20260921063506_add_missing_indexes_on_taggings.acts_as_taggable_on_engine.rb from acts_as_taggable_on_engine
+Copied migration 20260921063507_add_tenant_to_taggings.acts_as_taggable_on_engine.rb from acts_as_taggable_on_engine
